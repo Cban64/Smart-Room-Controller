8877fc1a0a08b7b5877d3f1413d53f4961dd4c22
Cban64 <cbanuelos1@cnm.edu>
2020-10-27 17:19:06 -0600

    bme/oled are working

--- Smart_Room_Controller/Smart_Room_Controller.ino
+++ Smart_Room_Controller/Smart_Room_Controller.ino
@@ -17,107 +17,77 @@
 #include <OneButton.h>
 #include <Encoder.h>
 
-
+//OLED
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET   4
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//BME
 Adafruit_BME280 bme;
-
-
 float varTempC;
 float varTempF;
-
 int status;
 
-OneButton button1 (buttonPin, false,false);// create object
+//buttons
+//OneButton button1 (buttonPin, false,false);// create object
 bool buttonState;
+
 EthernetClient client;
-bool status;
-
-
 
 void setup() {
   // Intialize:diode,encoder,onebutton
   //BME,and OLED
 
   Serial.begin(9600);
-  
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-      Serial.println(F("SSD1306 allocation failed"));
-      for(;;); // Don't proceed, loop forever
-    }
-  
-    // Show initial display buffer contents on the screen --
-    // the library initializes this with an Adafruit splash screen.
-    display.display();
-    delay(2000); // Pause for 2 seconds
-  
-    // Clear the buffer
-    display.clearDisplay();  
-  
-    // Show the display buffer on the screen. You MUST call display() after
-    // drawing commands to make them visible on screen!
-    display.display();
-    delay(2000);
-    // display.display() is NOT necessary after every single drawing command,
-    // unless that's what you want...rather, you can batch up a bunch of
-    // drawing operations and then update the screen all at once by calling
-    // display.display(). These examples demonstrate both approaches...
-   
-   
-   status = bme.begin(0x76);
-   if(status==false){
-    Serial.printf("failed to open BME");
-  
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
   }
   
- 
-
-}
-
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
   
-
-}
+  // Clear the buffer
+  display.clearDisplay();  
+  
+  status = bme.begin(0x76);
+  if(status==false){
+    Serial.printf("failed to open BME");
+  }
+} //end setup
 
 void loop() {
   //Automatic fnx: diode/encoder
   //For lighting control
+ 
+  //print temperature values to OLED
+  showTemp();    // Draw 'stylized' characters
+  
+}
 
-
-
-
+void showTemp(){    // Draw 'stylized' characters
   //Temp: sensor/display
   //Read value from sensors
   varTempC = bme.readTemperature();
   
  //convert Celsius to F 
   varTempF = ((varTempC*9)/5)+32;
-  
- 
-  //print values to OLED
-  testdrawstyles();    // Draw 'stylized' characters
+  display.clearDisplay();    
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(3,0);             // Start at top-left corner      
+  display.printf("Temp \n\n"); 
+  display.printf("%0.2f \n",varTempF);  
+  Serial.printf("Temp = %0.2f \n",varTempF);   
+  display.display();
+  delay(2000);
 }
-void testdrawstyles(){    // Draw 'stylized' characters
-
-      display.clearDisplay();    
-      display.setTextSize(1);             // Normal 1:1 pixel scale
-      display.setTextColor(SSD1306_WHITE);        // Draw white text
-      display.setCursor(0,0);             // Start at top-left corner      
-      display.printf("Temp = %0.2f \n",varTempF);  
-      Serial.printf("Temp = %0.2f \n",varTempF);   
-      display.display();
-      delay(2000);
-    
-  
-  }
-
-
-
 
   //Manual fnx: wemos/2buttons
   //comfort items (coffee/fan for demo)
   
-
-}
